6fa0bbde52ee650f88751669d1fd2e0f78c62798
theboss <luckybeastakatheboss@gmail.com>
2019-10-20 19:31:26 +0900

    Fix rebfav line-wraps

--- app/javascript/mastodon/components/account_mini.js
+++ app/javascript/mastodon/components/account_mini.js
@@ -6,24 +6,24 @@ import Permalink from '../components/permalink';
 
 export default class AccountMini extends ImmutablePureComponent {
 
-static propTypes = {
-  account: ImmutablePropTypes.map.isRequired,
-};
+  static propTypes = {
+    account: ImmutablePropTypes.map.isRequired,
+  };
 
-render () {
-  const { account } = this.props;
-  if (!account) {
-    return <div />;
-  }
-  return (
-    <div className='mini-avatar'>
-      <div className='account__avatar-wrapper' title={account.get('acct')}>
-        <Permalink key={account.get('id')} className='account__display-name' title={account.get('acct')} href={account.get('url')} to={`/accounts/${account.get('id')}`}>
-          <Avatar account={account} size={24} inline />
-        </Permalink>
+  render() {
+    const { account } = this.props;
+    if (!account) {
+      return <div />;
+    }
+    return (
+      <div className='mini-avatar'>
+        <div className='account__avatar-wrapper' title={account.get('acct')}>
+          <Permalink key={account.get('id')} className='account__display-name' title={account.get('acct')} href={account.get('url')} to={`/accounts/${account.get('id')}`}>
+            <Avatar account={account} size={24} inline />
+          </Permalink>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 }
